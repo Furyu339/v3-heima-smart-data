20117508cba30d238780d09115ff77ae4fdaec84
Furyu339 <xiejunfan95@gmail.com>
2024-12-06 17:26:50 +0800

    分页功能实现

--- src/views/car/CarCard.vue
+++ src/views/car/CarCard.vue
@@ -3,14 +3,31 @@
     <!-- 搜索区域 -->
     <div class="search-container">
       <span class="search-label">车牌号码：</span>
-      <el-input clearable placeholder="请输入内容" class="search-main" />
+      <el-input
+        v-model="params.carNumber"
+        clearable
+        placeholder="请输入内容"
+        class="search-main"
+      />
       <span class="search-label">车主姓名：</span>
-      <el-input clearable placeholder="请输入内容" class="search-main" />
+      <el-input
+        v-model="params.personName"
+        clearable
+        placeholder="请输入内容"
+        class="search-main"
+      />
       <span class="search-label">状态：</span>
-      <el-select style="width: 240px">
-        <el-option label="全部" value="1" />
+      <el-select v-model="params.cardStatus" style="width: 240px">
+        <el-option
+          v-for="item in cardStatusList"
+          :key="item.id"
+          :value="item.id"
+          :label="item.name"
+        />
       </el-select>
-      <el-button type="primary" class="search-btn">查询</el-button>
+      <el-button type="primary" class="search-btn" @click="doSearch">
+        查询
+      </el-button>
     </div>
     <!-- 新增删除操作区域 -->
     <div class="create-container">
@@ -47,7 +64,7 @@
         v-model:page-size="params.pageSize"
         :page-sizes="[10, 20, 30, 40]"
         size="default"
-        layout="total, prev, pager, next,  sizes"
+        layout="total, prev, pager, next, sizes"
         :total="total"
         @current-change="pageChange"
         @size-change="sizeChangeFn"
@@ -57,9 +74,12 @@
 </template>
 
 <script lang="ts" setup>
+// 导入所需的 API 和类型
 import { getCardListAPI } from "@/apis/card";
 import type { Card, CardListParams } from "@/types/card";
+import { ref, onMounted } from "vue";
 
+// 定义加载状态
 const loading = ref(false);
 
 const params = ref<CardListParams>({
@@ -67,29 +87,68 @@ const params = ref<CardListParams>({
   pageSize: 10,
   carNumber: undefined, // 车辆号码
   personName: undefined, // 车主姓名
-  cardStatus: undefined, // 车辆状态
+  cardStatus: undefined, // 车辆状态 
 });
-// 月卡列表
+
+// 定义月卡列表和总数
 const cardList = ref<Card[]>([]);
 const total = ref(0);
+
+// 定义卡片状态列表
+const cardStatusList = [
+  {
+    id: -1,
+    name: "全部",
+  },
+  {
+    id: 0,
+    name: "可用",
+  },
+  {
+    id: 1,
+    name: "已过期",
+  },
+];
+
+// 获取月卡列表的函数
 const getCardList = async () => {
   loading.value = true;
-  const res = await getCardListAPI(params.value);
+  const tempParams = { ...params.value };
+  console.log('tempParams',tempParams);
+  if (Number(tempParams.cardStatus) === -1) {
+    delete tempParams.cardStatus;
+  }
+
+  const res = await getCardListAPI(tempParams);
   cardList.value = res.data.rows;
   total.value = res.data.total;
   loading.value = false;
 };
+
+// 页码变化的处理函数
 const pageChange = (page: number) => {
   params.value.page = page;
   getCardList();
 };
+
+// 每页条数变化的处理函数
 const sizeChangeFn = (pageSize: number) => {
   params.value.pageSize = pageSize;
   getCardList();
 };
+
+// 组件挂载时获取月卡列表
 onMounted(() => {
   getCardList();
 });
+
+// 查询按钮的处理函数
+const doSearch = () => {
+  // 调用接口之前把页数参数重置为 1 
+  // （防止页码在其他页时，搜索时数据不够，导致还在原来页码停留）
+  params.value.page = 1;
+  getCardList();
+};
 </script>
 
 <style lang="scss" scoped>
@@ -101,7 +160,7 @@ onMounted(() => {
 .search-container {
   display: flex;
   align-items: center;
-  border-bottom: 1px solid rgb(237, 237, 237, 0.9);
+  border-bottom: 1px solid rgba(237, 237, 237, 0.9);
   padding-bottom: 20px;
 
   .search-main {
@@ -115,15 +174,19 @@ onMounted(() => {
 }
 
 .create-container {
-  margin: 10px 0px;
+  margin: 10px 0;
+}
+
+.table {
+  margin-top: 20px;
 }
 
 .page-container {
-  padding: 4px 0px;
+  padding: 4px 0;
   text-align: right;
 }
 
 .form-container {
-  padding: 0px 80px;
+  padding: 0 80px;
 }
 </style>
